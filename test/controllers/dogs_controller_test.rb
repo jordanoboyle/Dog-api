0a98dbbfa93ba78d51815945c3071c9d641a76c1
jordanoboyle <jmcmcode@gmail.com>
2024-05-16 23:36:30 -0600

    all tests pass

--- test/controllers/dogs_controller_test.rb
+++ test/controllers/dogs_controller_test.rb
@@ -6,39 +6,39 @@ class DogsControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test "index" do
-    get "/products.json"
+    get "/dogs.json"
     assert_response 200
   
     data = JSON.parse(response.body)
-    assert_equal Product.count, data.length
+    assert_equal Dog.count, data.length
   end
   
   test "create" do
-    assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description"  }
+    assert_difference "Dog.count", 1 do
+      post "/dogs.json", params: { species: "test dog", color: "1", weight: 1, image_url: "test description"  }
     end
   end
   
   test "show" do
-    get "/products/#{Product.first.id}.json"
+    get "/dogs/#{Dog.first.id}.json"
     assert_response 200
   
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "species", "color", "weight", "image_url", "created_at", "updated_at"], data.keys
   end
   
   test "update" do
-    photo = Photo.first
-    patch "/photos/#{photo.id}.json", params: { name: "Updated name" }
+    dog = Dog.first
+    patch "/dogs/#{dog.id}.json", params: { species: "Updated name" }
     assert_response 200
   
     data = JSON.parse(response.body)
-    assert_equal "Updated name", data["name"]
+    assert_equal "Updated name", data["species"]
   end
   
   test "destroy" do
-    assert_difference "Photo.count", -1 do
-      delete "/photos/#{Photo.first.id}.json"
+    assert_difference "Dog.count", -1 do
+      delete "/dogs/#{Dog.first.id}.json"
       assert_response 200
     end
   end
